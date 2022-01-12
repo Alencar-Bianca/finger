e218120cf598644e9da6f562660c3fc20a68901b
Alencar-Bianca <alencar-bianca@hotmail.com>
2022-01-12 01:24:45 -0300

    Finger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    LoginComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
